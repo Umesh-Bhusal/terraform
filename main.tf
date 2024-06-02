cc0a5276922f176d736a8ca49d190730e167d9e4
umesh <umeshrewasoft.com>
2024-06-02 21:36:08 +0545

    adding

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
 resource aws_instance "terraform"{
-    ami = "ami-0705384c0b33c194c"
+    ami = "ami-04b70fa74e45c3917"
     instance_type = "t2.micro"
 
-tags{
-    Name= terraform-example
+    user_data = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
+user_data_replace_on_change = true
+
+tags = {
+    Name = "terraform-example"
 }
 }
